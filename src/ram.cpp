1c020bc337543921dcd3f1c165b48cee8b84dcfb
Matt Comben <matthew@dockstudios.co.uk>
2019-05-15 06:59:35 +0100

    Add RAM file and remove binary

---
+++ src/ram.cpp
@@ -0,0 +1,5 @@
+class RAM {
+    // @TODO Reduce this to exclude mappings to other
+    // periferals.
+    memory = std::vector<uint8_t>(0x10000);
+}
---
+++ src/ram.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+class RAM {
+
+private:
+    std::vector<uint8_t> memory;
+ };
