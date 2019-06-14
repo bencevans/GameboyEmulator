db5eb43d80b7f4b7369f6102197ceed5327ef60d
Matt Comben <matthew@dockstudios.co.uk>
2019-06-14 13:30:02 +0100

    Improve some code styles

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,13 +14,15 @@
 #define DISABLE_VPU 0
 #define DEBUG 0
 
-struct arguments_t {
+struct arguments_t
+{
     char *bios_path;
     char *rom_path;
     bool valid;
 };
 
-arguments_t get_arguments(int argc, char* args[]) {
+arguments_t get_arguments(int argc, char* args[])
+{
     arguments_t arguments;
     if (argc != 3) {
         std::cout << "Usage: GameboyEmulator <BIOS path> <ROM path>" << std::endl;
@@ -35,35 +37,9 @@ arguments_t get_arguments(int argc, char* args[]) {
     return arguments;
 }
 
-//void handle_error() {
-//    CPU::print_state();
-//};
-
-//class StaticState {
-//public:
-//    StaticState(CPU cpu_inst_);
-//    static CPU get_inst();
-//};
-//StaticState::StaticState(CPU cpu_inst_) {
-//    static CPU StaticState::cpu_inst = cpu_inst_;
-//}
-//CPU StaticState::get_inst() {
-//    return StaticState::cpu_inst;
-//}
 
 int main(int argc, char* args[])
 {
-    //arguments_t arguments = get_arguments(argc, args);
-    //if (arguments.valid == false) {
-    //    return 1;
-    //}
-
-    // create the window
-//    Display *di = XOpenDisplay(getenv("DISPLAY"));
-//    if (di == NULL) {
-//		printf("Couldn't open display.\n");
-//		return -1;
-//	}
     Helper::init();
     RAM *ram_inst = new RAM();
     VPU *vpu_inst = new VPU(ram_inst);
@@ -77,8 +53,8 @@ int main(int argc, char* args[])
     cpu_inst->reset_state();
 
     // Load bios/RAM
-    //char bios_path[] = "./copyright/DMG_ROM.bin";
-    char bios_path[] = "./matt-test-daa.rom";
+    char bios_path[] = "./copyright/DMG_ROM.bin";
+    //char bios_path[] = "./matt-test-daa.rom";
     //char rom_path[] = "./copyright/Tetris (JUE) (V1.1) [!].gb";
     //char rom_path[] = "./resources/test_roms/cpu_instrs/cpu_instrs.gb";
     
@@ -86,7 +62,7 @@ int main(int argc, char* args[])
     // 01-special.gb
     // 02-interrupts.gb
     // 03-op sp,hl.gb
-    // 04-op r,imm.gb - fail (ce de)
+    // 04-op r,imm.gb - fail (de)
     // 05-op rp.gb -passed
     // 06-ld r,r.gb - pased
     // 07-jr,jp,call,ret,rst.gb
@@ -95,36 +71,22 @@ int main(int argc, char* args[])
     // 10-bit ops.gb -- passed
     // 11-op a,(hl).gb
 
-    char rom_path[] = "./resources/test_roms/cpu_instrs/individual/11-op a,(hl).gb";
+    char rom_path[] = "./resources/test_roms/cpu_instrs/individual/07-jr,jp,call,ret,rst.gb";
     ram_inst->load_bios(bios_path);
     ram_inst->load_rom(rom_path);
 
-    // Setup handler for exception
-    //void (*prev_handler)(int);
-    //prev_handler =
-    //signal(SIGSEGV, CPU::print_state);
-
-    // run the program as long as the window is open
+   // run the program as long as the window is open
     bool to_vpu_tick = true;
     while (cpu_inst->is_running())
     {
         cpu_inst->tick();
-        if (! DISABLE_VPU) {
+        if (! DISABLE_VPU)
+	{
             if (to_vpu_tick)
+	    {
                 vpu_inst->tick();
+	    }
             to_vpu_tick = ! to_vpu_tick;
-            //vpu_inst->tick();
-            //vpu_inst->tick();
-            //vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
-//            vpu_inst->tick();
         }
 
         //vpu_inst->process_events();
